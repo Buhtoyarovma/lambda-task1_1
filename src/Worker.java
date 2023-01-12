45258577d6b1bcf4afe7f52e8c797404c9ecdfe1
Mikhail Bukhtoyarov <m.buhtoyarov@yandex.ru>
2023-01-12 23:56:09 +0300

    add gitignore

--- src/Worker.java
+++ src/Worker.java
@@ -5,15 +5,29 @@ public class Worker {
         void onDone(String result);
     }
 
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+
+
     private OnTaskDoneListener callback;
 
-    public Worker(OnTaskDoneListener callback) {
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
         this.callback = callback;
+        this.errorCallback = errorCallback;
+
     }
 
     public void start() {
         for (int i = 0; i < 100; i++) {
-            callback.onDone("Task " + i + " is done");
+            if (i == 33) {
+                errorCallback.onError("Error! Task " + i + " is not done");
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
         }
     }
 
